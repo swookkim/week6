c868b3cb2ebb8e867c0ce53fec5c4a3ea04e3c2d
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-04 09:06:18 -0500

    1

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -4,6 +4,6 @@ exports.handler = async function(event) {
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: `Hello from the back-end!!` // a string of data
   }
 }
